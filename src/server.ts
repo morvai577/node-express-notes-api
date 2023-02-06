fa222cf071ff471c76d4dbc9c919c51c248572d5
morvai577 <thevaimor@icloud.com>
2023-02-06 20:28:22 +1300

    Add middleware and register router

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import morgan from 'morgan'
+import router from './router.js'
+
+const app = express()
+
+// Register middleware
+app.use(morgan('dev'))
+app.use(express.json()) // Parse JSON bodies
+// app.use(express.urlencoded({extended: true})) // Parse URL-encoded bodies
+
+// Register router for API url
+app.use('/api', router)
+
+export default app
