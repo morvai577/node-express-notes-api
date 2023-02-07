7fffccdbc97a56c5c6ef0d2a7b37af47954ec849
morvai577 <thevaimor@icloud.com>
2023-02-07 17:25:19 +1300

    Update createNote handler
    For new required note title

--- src/handlers/note.ts
+++ src/handlers/note.ts
@@ -4,6 +4,7 @@ import prisma from '../db.js'
 export const createNote = async (req: Request, res: Response) => {
   const note = await prisma.note.create({
     data: {
+      title: req.body.title,
       content: req.body.content,
     },
   })
