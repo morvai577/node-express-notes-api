94bedf031b22f70f5a3ae34d78c8984509a3593c
morvai577 <thevaimor@icloud.com>
2023-02-07 17:38:56 +1300

    Add new handler to get all notes

--- src/handlers/note.ts
+++ src/handlers/note.ts
@@ -1,6 +1,18 @@
 import {Request, Response} from 'express'
 import prisma from '../db.js'
 
+export const getNotes = async (req: Request, res: Response) => {
+  const notes = await prisma.note.findMany({
+    select: {
+      id: true,
+      title: true,
+      content: false,
+    },
+  })
+
+  res.json({data: notes})
+}
+
 export const createNote = async (req: Request, res: Response) => {
   const note = await prisma.note.create({
     data: {
