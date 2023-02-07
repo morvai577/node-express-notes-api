07a1b40bfa90b45154dc3aa5df0ea5f0ecb8c61f
morvai577 <thevaimor@icloud.com>
2023-02-07 17:39:18 +1300

    Add new route to get all notes

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express'
-import {createNote} from './handlers/note.js'
+import {createNote, getNotes} from './handlers/note.js'
 
 const router: Router = Router()
 
 /**
  * Note
  */
+router.get('/note', getNotes)
 router.post('/note', createNote)
 
 export default router
