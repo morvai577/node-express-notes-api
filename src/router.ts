00f161002b4c44cee6c6bc974398cf067c2bda22
morvai577 <thevaimor@icloud.com>
2023-02-09 09:23:01 +1300

    Add new route to get a note based on id

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express'
-import {createNote, deleteNote, getNotes} from './handlers/note.js'
+import {createNote, deleteNote, getNote, getNotes} from './handlers/note.js'
 
 const router: Router = Router()
 
 /**
  * Note
  */
+router.get('/note/:id', getNote)
 router.get('/note', getNotes)
 router.post('/note', createNote)
 router.delete('/note/:id', deleteNote)
